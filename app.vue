0da57d0ca334ce26703bc828ce87ead9f5b87fb8
jonmes <mesfinjohn@rocketmail.com>
2022-07-08 06:09:15 +0300

    heroicon & headless & html title added

--- app.vue
+++ app.vue
@@ -1,3 +1,17 @@
+<script>
+useHead({
+  title: "HaHu Jobs",
+  // or, instead:
+  // titleTemplate: (title) => `My App - ${title}`,
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+  meta: [{ name: "description", content: "HaHu Jobs" }],
+  bodyAttrs: {
+    class: "test",
+  },
+});
+</script>
+
 <template>
   <div>
     <NuxtPage />
