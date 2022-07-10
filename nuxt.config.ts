496370771395c8beb31d510132b8c1fe0add6b12
jonmes <mesfinjohn@rocketmail.com>
2022-07-10 07:02:16 +0300

    font added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -106,10 +106,10 @@ export default defineNuxtConfig({
         },
         { href: "https://www.google-analytics.com", rel: "preconnect" },
 
-        // {
-        //   href: "https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;600;700;800;900&display=swap",
-        //   rel: "stylesheet",
-        // },
+        {
+          href: "https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;600;700;800;900&display=swap",
+          rel: "stylesheet",
+        },
       ],
       script: [
         {
